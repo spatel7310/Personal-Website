790f507b56b072800c0de5c235dca260d1fb7e84
Sheev Patel <spatel8291@gmail.com>
2023-04-27 00:44:32 -0400

    Initial commit, intialized project and created base header

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/sections/home/home.component';
+import { HeaderComponent } from './components/sections/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
