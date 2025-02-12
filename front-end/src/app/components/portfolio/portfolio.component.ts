f95102670b8194ffe36942cb2f9f1b1f99723536
Sheev Patel <spatel8291@gmail.com>
2025-02-11 21:25:38 -0500

    creating starting block for the portfolio section

--- front-end/src/app/components/portfolio/portfolio.component.ts
+++ front-end/src/app/components/portfolio/portfolio.component.ts
@@ -5,9 +5,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+  images: string[] = [
+    '../../../assets/img/code_builder.png',
+    '../../../assets/img/phone_emulator.png',
+    '../../../assets/img/soldby.jpeg',
+    '../../../assets/img/phone_emulator.png',
+  ];
+  currentImageIndex: number = 0;
+  intervalId: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.startSlideshow();
+  }
+
+  startSlideshow(): void {
+    this.intervalId = setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    }, 3000); // Change image every 3 seconds
+  }
+
+  onSlideClick(): void {
+    if (this.currentImageIndex === 0) {
+      // Open the website in a new tab
+      window.open('https://soldbysulay.herokuapp.com/', '_blank');
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 }
