2212da399d39040524491c778ad6487c2cd511ed
Sheev Patel <spatel8291@gmail.com>
2025-02-20 18:51:11 -0500

    Adding image to portfolio page and reponsive edits, cleaning up the phone emulator

--- front-end/src/app/components/portfolio/portfolio.component.ts
+++ front-end/src/app/components/portfolio/portfolio.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent implements OnInit, OnDestroy {
   images: string[] = [
     '../../../assets/img/code_builder.png',
     '../../../assets/img/soldby.jpeg',
     '../../../assets/img/notely.png',
-    '../../../assets/img/soldby.jpeg'
+    '../../../assets/img/resume_site.jpeg'
   ];
   currentImageIndex: number = 0;
   intervalId: any;
@@ -28,10 +28,13 @@ export class PortfolioComponent implements OnInit {
   }
 
   onSlideClick(): void {
-    if (this.currentImageIndex === 0) {
-      // Open the website in a new tab
-      window.open('https://soldbysulay.herokuapp.com/', '_blank');
-    }
+    const urls = [
+      'https://github.com/spatel7310/Urban-Science-Code-Builder',
+      'https://soldbysulay.herokuapp.com/',
+      'https://github.com/spatel7310/Notely',
+      'https://github.com/spatel7310/Personal-Website'
+    ];
+    window.open(urls[this.currentImageIndex], '_blank');
   }
 
   ngOnDestroy(): void {
