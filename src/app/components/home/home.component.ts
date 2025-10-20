d16d80c08e6e37873492b0a5653fb81c11a68f47
Sheev Patel <sheevpatel@MacBookPro.lan>
2025-10-19 22:40:55 -0400

    typewriter bug in prod

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,9 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor() {
-  }
+  private fullText: string = "there is nothing artificial about my intelligence. let's work together to transform ideas into exceptional software";
+  public displayedText: string = "";
+  private index: number = 0;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.typeWriter();
+  }
+
+  private typeWriter(): void {
+    const interval = setInterval(() => {
+      if (this.index < this.fullText.length) {
+        this.displayedText += this.fullText.charAt(this.index);
+        this.index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 100); // Adjust typing speed here
   }
 }
